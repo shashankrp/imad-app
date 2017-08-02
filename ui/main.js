cfdd671cdffb67ce7542a0dd3592dc17bd011acd
shashankrp <30649674+shashankrp@users.noreply.github.com>
2017-08-02 15:56:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ element.innerHTML= 'New value';
 var img=document.getElementById('madi');
 var marginLeft = 0;
 function movRigth(){
-    marginLeft = marginLeft + 5;
+    marginLeft = marginLeft + 1;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick=function(){
