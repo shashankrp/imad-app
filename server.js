8f6aeb0d197d72a7cce71f39ea59e1f74f790c2b
shashankrp <30649674+shashankrp@users.noreply.github.com>
2017-08-02 14:14:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,17 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/article-one',function(req,res){
+   res.send('Article-one requiested and will be served here'); 
+});
 
+app.get('/article-two',function(req,res){
+   res.send('Article-two requiested and will be served here'); 
+});
+
+app.get('/article-three',function(req,res){
+   res.send('Article-three requiested and will be served here'); 
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
