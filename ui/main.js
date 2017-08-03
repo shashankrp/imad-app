53e39f29fc84b48ec40cbc7d86306667cc3178db
shashankrp <30649674+shashankrp@users.noreply.github.com>
2017-08-03 10:41:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,3 +19,26 @@ button.onclick = function(){
     request.open('GET','http://shashankrp2.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+
+//cubmit name
+var namerInupt = document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick = function() {
+    //make a request to the serverm and send the name
+    
+    
+    //Capture a list of names and render it as a list
+var names=['name1' , 'name2' , 'name3','name4'];
+var list = '';
+for (var i=0;i<names.length;i++)
+{
+    list += '<li>' +name[i]+ '</li>';
+}
+ var ul=document.getElementById('namelist');
+ ul.innerHTML = list;
+    
+};
+
+
+
