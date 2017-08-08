2589bd6c081fcaa138a486d0f76d9a18bef21d52
shashankrp <30649674+shashankrp@users.noreply.github.com>
2017-08-08 15:45:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,19 +20,36 @@ button.onclick = function(){
     request.send(null);
 };
 
+
+
 //cubmit name
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
-var names=['name1' , 'name2' , 'name3' , 'name4'];
-var list = '';
-for (var i=0 ; i< names.length ; i++){
-    list += '<li>' + names[i] + '</li>';
-}
- var ul=document.getElementById('namelist');
- ul.innerHTML = list;
+     var request = new XMLHttpRequest();
     
+    //capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                var names = request.responseText;
+                names=JSON.parse(names);
+                var list = '';
+                for (var i=0 ; i< names.length ; i++){
+                list += '<li>' + names[i] + '</li>';
+                 }
+            var ul=document.getElementById('namelist');
+            ul.innerHTML = list;
+
+            }
+        }
+        //Not done yet
+    };
+    //Make the request
+    request.open('GET','http://shashankrp2.imad.hasura-app.io/submit-name?name=' +name,true);
+    request.send(null);
+
 };
 
 
