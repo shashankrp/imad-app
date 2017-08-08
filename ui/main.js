004646744497873a8e2365e059ca76f9f1c59b71
shashankrp <30649674+shashankrp@users.noreply.github.com>
2017-08-08 15:25:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,14 +21,13 @@ button.onclick = function(){
 };
 
 //cubmit name
-var namerInput = document.getElementById('name');
-var name=nameInput.value;
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
 var names=['name1' , 'name2' , 'name3' , 'name4'];
 var list = '';
-for (var i=0;i<names.length;i++)
-{
+for (var i=0 ; i< names.length ; i++){
     list += '<li>' + names[i] + '</li>';
 }
  var ul=document.getElementById('namelist');
