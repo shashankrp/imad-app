67313fed18c6a382255ec6422d04b0e549005e34
shashankrp <30649674+shashankrp@users.noreply.github.com>
2017-08-08 21:39:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var config = {
     database:'shashankrp2',
     host: 'db.imad.hasura-app.io',
     port:'5432',
-    password: process.env.DB_PASSWORD
+    password: process.env.DB_PASSWORD,
 };
 
 
